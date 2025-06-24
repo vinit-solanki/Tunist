0d2252e02691351109e3d7129a445cac502c9dba
vinit-solanki <2022.vinit.solanki@ves.ac.in.com>
2025-06-24 18:33:32 +0530

    Update in routes.ts

--- user_service/src/routes.ts
+++ user_service/src/routes.ts
@@ -1,8 +1,8 @@
 import express from 'express'
 import { loginUser, myProfile, registerUser, getUser } from './controller.js'
 import {isAuth} from './middleware.js'
-const router = require("express").Router();
-
+import { Router } from 'express'
+const router = express.Router();
 router.post("/user/register",registerUser)
 router.post("/user/login",loginUser)
 router.get("/user/me", isAuth, myProfile)
